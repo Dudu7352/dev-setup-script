1ffd195f4330765e4ca1b9855900e26eb2aeee83
Dudu7352 <aeiou7352@gmail.com>
2023-09-07 20:32:52 +0200

    Fixed and updated setup_code.ps1

--- setup_code.ps1
+++ setup_code.ps1
@@ -1,6 +1,19 @@
-$vscodeURI = "https://az764295.vo.msecnd.net/stable/6c3e3dba23e8fadc360aed75ce363ba185c49794/VSCode-win32-x64-1.81.1.zip"
+$vsCodeUrl = "https://az764295.vo.msecnd.net/stable/6c3e3dba23e8fadc360aed75ce363ba185c49794/VSCode-win32-x64-1.81.1.zip"
+$vsCodeZip = $PSScriptRoot + "\vscode-install.zip"
+$vsCodePath = $PSScriptRoot + "\VSCode"
+Import-Module BitsTransfer
 
 if(-not(Test-Path $PSScriptRoot\VSCode)) 
 {
-    Write-Output (Invoke-WebRequest -URI $vscodeURI).Content -OutFile $PSScriptRoot\vscode-install.zip
+    Write-Output "Downloading VSCode zip file..."
+    Start-BitsTransfer -Source $vsCodeUrl -Destination $vsCodeZip
+    Expand-Archive -LiteralPath $vsCodeZip -DestinationPath $vsCodePath
+    Write-Output "Adding data folder..."
+    New-Item $vsCodePath\data -ItemType Directory
+    Write-Output "Removing temporary files..."
+    Remove-Item $vsCodeZip
+    Write-Output "VSCode install complete"
+}
+else {
+    Write-Output "VSCode seems to be already present. Omitting vscode install"
 }
